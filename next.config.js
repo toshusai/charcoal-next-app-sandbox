8eef0b722019d95ccc0d658d41b250e37b60ed5b
toshusai <toshusai.net@gmail.com>
2023-01-10 11:41:01 +0900

    init

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  compiler: {
+    styledComponents: true,
+  },
+  experimental: {
+    esmExternals: false
+  }
+}
+
+module.exports = nextConfig
